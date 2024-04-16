35447f2d117c59ddf6868198f4e9587827e7087a
M-Roesner <markus_roesner@web.de>
2024-04-16 22:24:45 +0200

    Adjustment of internal imports.

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,9 @@
-// Styled-Component
-import StyledHeader from "./styledHeader";
-
 // Components
 import NavWrapper from "./navigation/NavWrapper";
 import Logo from "./logo/Logo";
+
+// Styled-Component
+import StyledHeader from "./styledHeader";
 
 const Header = () => {
   return (

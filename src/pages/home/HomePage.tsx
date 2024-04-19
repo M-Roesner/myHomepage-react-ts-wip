c2e308e343b9724a0a95b28c31e9543d18e41794
M-Roesner <markus_roesner@web.de>
2024-04-19 22:05:53 +0200

    Changed test text on the page home with styled cards.

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,9 +1,13 @@
+import CardHeadline from "../../components/custom/card/cardHeadline/CardHeadline";
+import CardVerticalLine from "../../components/custom/card/cardVerticalLine/CardVerticalLine";
 import StyledHomePage from "./styledHomePage";
 
 const HomePage = () => {
   return (
     <StyledHomePage>
-      <h1>Home Page</h1>
+      <CardVerticalLine>
+        <CardHeadline level={1}>Home Page</CardHeadline>
+      </CardVerticalLine>
     </StyledHomePage>
   );
 };

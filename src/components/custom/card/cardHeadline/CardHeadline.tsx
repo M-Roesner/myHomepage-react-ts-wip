997d6bdc267d8f19388f1690402e9a76cebde01d
M-Roesner <markus_roesner@web.de>
2024-04-19 22:02:33 +0200

    Created the card structure for the headline design.

---
+++ src/components/custom/card/cardHeadline/CardHeadline.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import {
+  StyledCardHeadline1,
+  StyledCardHeadline2,
+  StyledCardHeadline3,
+  StyledCardHeadline4,
+  StyledCardHeadline5,
+  StyledCardHeadline6,
+} from "./styledCardHeadline";
+
+type CardHeadlineProps = {
+  level: 1 | 2 | 3 | 4 | 5 | 6;
+  children: React.ReactNode;
+};
+
+/**
+ * Diese Funktion gibt eine Referenz auf die entsprechende Styled-Card-Header-Komponente zurück,
+ * basierend auf dem übergebenen Level.
+ *
+ * @param level - Der Header-Level, der angibt, welcher Styled-Card-Header gerendert werden soll.
+ * @returns - Eine Funktionsreferenz zur entsprechenden Styled-Card-Header-Komponente.
+ *
+ * Diese Funktion ermöglicht eine flexible Verwendung der Styled-Card-Header-Komponenten,
+ * indem sie die Funktionsreferenz zurückgibt, um die Komponente später zu rendern, wenn sie benötigt wird.
+ * Dadurch wird die Komponente nicht sofort gerendert, sondern kann dynamisch basierend auf dem übergebenen Level gerendert werden.
+ */
+const getCardHeadlineComponent = (level: CardHeadlineProps["level"]) => {
+  switch (level) {
+    case 1:
+      // return <StyledCardHeadline1 />;
+      // Diese Syntax würde eine Instanz der StyledCardHeadline1-Komponente erstellen und sie sofort rendern.
+      // Es soll aber nur eine Referenz zur Komponente zurückgeben werden, um sie später zu rendern.
+      return StyledCardHeadline1;
+    case 2:
+      return StyledCardHeadline2;
+    case 3:
+      return StyledCardHeadline3;
+    case 4:
+      return StyledCardHeadline4;
+    case 5:
+      return StyledCardHeadline5;
+    case 6:
+      return StyledCardHeadline6;
+    default:
+      return StyledCardHeadline1;
+  }
+};
+
+/**
+ * Functional component for rendering a card header with a specified level 1-6 and including any children content.
+ *
+ * @param {CardHeadlineProps} props - The props object containing the header level 1-6 and children.
+ */
+const CardHeadline = ({ level, children }: CardHeadlineProps) => {
+  const Header = getCardHeadlineComponent(level);
+  return <Header>{children}</Header>;
+};
+
+export default CardHeadline;
---
+++ src/components/custom/card/cardHeadline/styledCardHeadline.ts
@@ -0,0 +1,56 @@
+import styled from "styled-components";
+
+const defaultHeaderStyles = `
+// font-weight: 200;
+// font-style: normal;
+line-height: 3ch;
+`;
+
+// TODO: header styles ... content, color ...
+export const StyledCardHeadline1 = styled.h1`
+  ${defaultHeaderStyles};
+  color: #5893c4;
+  font-size: 2rem;
+  margin-top: 0.6ch;
+  &::after {
+    content: "{}";
+    color: #f5f4f396;
+  }
+`;
+
+export const StyledCardHeadline2 = styled.h2`
+  ${defaultHeaderStyles};
+  font-size: 1.6rem;
+  color: #d5d36c;
+  &::after {
+    content: "()";
+    color: #d5d36c;
+  }
+`;
+
+export const StyledCardHeadline3 = styled.h3`
+  ${defaultHeaderStyles};
+  font-size: 1.6rem;
+  color: #06b300;
+  &::before {
+    content: "// ";
+    color: #06b300;
+  }
+`;
+
+export const StyledCardHeadline4 = styled.h4`
+  ${defaultHeaderStyles};
+  font-size: 1.4rem;
+  color: #f5f4f396;
+`;
+
+export const StyledCardHeadline5 = styled.h5`
+  ${defaultHeaderStyles};
+  font-size: 1.2rem;
+  color: #f5f4f396;
+`;
+
+export const StyledCardHeadline6 = styled.h6`
+  ${defaultHeaderStyles};
+  font-size: 1rem;
+`;

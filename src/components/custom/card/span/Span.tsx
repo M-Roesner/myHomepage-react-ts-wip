0bb0356c6a9b059cf9ed4ed2ab6566a6450d01eb
M-Roesner <markus_roesner@web.de>
2024-04-20 15:12:54 +0200

    Added custom Span componant.

---
+++ src/components/custom/card/span/Span.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { StyledSpan } from "./styledSpan";
+
+type SpanProps = {
+  title: string;
+  children: React.ReactNode;
+};
+
+/**
+ * Renders a styled span tag with a title and a child.
+ * Additionally, there is a white space at the beginning and end of the span tag.
+ *
+ * @param {SpanProps} props - The props object contains the title and children.
+ */
+const Span = ({ title, children }: SpanProps) => {
+  return (
+    <>
+      {" "}
+      <StyledSpan title={title}>{children}</StyledSpan>{" "}
+    </>
+  );
+};
+
+export default Span;
---
+++ src/components/custom/card/span/styledSpan.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const StyledSpan = styled.span`
+  text-decoration: underline dotted #b8610f;
+  cursor: help;
+`;

1bef32ebfba75b76e2068ac3c1cc4b04a81a5d32
M-Roesner <markus_roesner@web.de>
2024-04-27 10:09:04 +0200

    Attempts to implement useRouteError with typescript. Code commented out!

--- src/pages/errorPage/NotFoundPage.tsx
+++ src/pages/errorPage/NotFoundPage.tsx
@@ -1,8 +1,17 @@
-import { Link } from "react-router-dom";
+import {
+  Link,
+  // , useRouteError
+} from "react-router-dom";
 
 export default function NotFoundPage() {
+  // TODO: Types for react router and adding error handling
+  // npm i --save @types/react-router
+  // npm i --save @types/react-router-dom
+  // const error = useRouteError();
+  // console.error(error);
   return (
     <div className="flex flex-col gap-2">
+      <p>{/* <i>{error.statusText || error.message}</i> */}</p>
       Error: 404 Page Not Found
       <br />
       <Link to="/">Zur Hauptseite</Link>

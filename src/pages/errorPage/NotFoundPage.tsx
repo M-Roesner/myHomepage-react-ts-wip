e8723557a4bbda8f49977a495686f82f25887a46
M-Roesner <markus_roesner@web.de>
2024-04-27 10:38:11 +0200

    Added error handling for error page with my own typing.

--- src/pages/errorPage/NotFoundPage.tsx
+++ src/pages/errorPage/NotFoundPage.tsx
@@ -1,18 +1,29 @@
-import {
-  Link,
-  // , useRouteError
-} from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
 
+type ErrorProps = {
+  statusText?: string;
+  message?: string;
+  status?: number | string;
+  data?: string;
+  internal?: boolean;
+  error?: {
+    message?: string;
+    stack?: string;
+  };
+};
 export default function NotFoundPage() {
   // TODO: Types for react router and adding error handling
+  // Added my own typing for handling the error that I know.
   // npm i --save @types/react-router
   // npm i --save @types/react-router-dom
-  // const error = useRouteError();
-  // console.error(error);
+  const error = useRouteError() as ErrorProps;
+  console.error(error);
   return (
     <div className="flex flex-col gap-2">
-      <p>{/* <i>{error.statusText || error.message}</i> */}</p>
-      Error: 404 Page Not Found
+      {error.status && <p>{`status: '${error.status}'`}</p>}
+      {error.statusText && <p>{`statusText: '${error.statusText}'`}</p>}
+      {/* {error.data && <p>{`data: '${error.data}'`}</p>} */}
+      {error?.error?.message && <p>{`message: '${error.error.message}'`}</p>}
       <br />
       <Link to="/">Zur Hauptseite</Link>
     </div>

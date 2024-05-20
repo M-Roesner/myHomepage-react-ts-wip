4ae28ac47d7728bbe9309471c7e3c551c0a524b2
M-Roesner <markus_roesner@web.de>
2024-05-20 23:28:18 +0200

    The usage of the sort function has been replaced by an imported sortList function.

--- src/pages/aboutMe/sections/skills/skillListWrapper/SkillListWrapper.tsx
+++ src/pages/aboutMe/sections/skills/skillListWrapper/SkillListWrapper.tsx
@@ -5,6 +5,7 @@ import SkillList from "./skillList/skillList";
 
 // Resources, helpers and types
 import { mySampleSkills } from "../../../../../resources/samples/skills/mySampleSkills";
+import { sortListByKeyType } from "../../../../../utils/sortListByKeyType";
 import { SkillType } from "../skillTypes";
 import { ELanguage, formatDate } from "../../../../../utils/dateHelper";
 
@@ -36,10 +37,10 @@ const findLastDateOfSkillList = (skillList: SkillType[]): Date => {
  */
 export const SkillListWrapper = () => {
   // TODO: SkillList data receiving: This structure for receiving data will be changed with the database in future.
-  const frontendSkillList = mySampleSkills["Frontend"].sort((a, b) => a.order - b.order);
-  const backendSkillList = mySampleSkills["Backend"].sort((a, b) => a.order - b.order);
-  const designSkillList = mySampleSkills["Design"].sort((a, b) => a.order - b.order);
-  const otherSkillList = mySampleSkills["Other"].sort((a, b) => a.order - b.order);
+  const frontendSkillList = sortListByKeyType(mySampleSkills["Frontend"], "order", "acc");
+  const backendSkillList = sortListByKeyType(mySampleSkills["Backend"], "order", "acc");
+  const designSkillList = sortListByKeyType(mySampleSkills["Design"], "order", "acc");
+  const otherSkillList = sortListByKeyType(mySampleSkills["Other"], "order", "acc");
 
   const lastDateArray: Date[] = [];
   lastDateArray.push(findLastDateOfSkillList(mySampleSkills["Frontend"]));

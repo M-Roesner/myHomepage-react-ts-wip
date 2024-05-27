ba8536ca36637740eb5c78a612cf8dd147fbcb70
M-Roesner <markus_roesner@web.de>
2024-05-27 20:15:04 +0200

    Code clean up and added ToDo.

--- src/pages/main/index.ts
+++
@@ -1,9 +0,0 @@
-import App from "./App.tsx";
-import NotFoundPage from "../errorPage/NotFoundPage.tsx";
-import AboutMePage from "../aboutMe/AboutMe.tsx";
-import HomePage from "../home/HomePage.tsx";
-import MyPortfolio from "../myPortfolio/MyPortfolio.tsx";
-import ImprintPage from "../Imprint/ImprintPage.tsx";
-import Footer from "../../components/footer/footer.tsx";
-
-export { App, NotFoundPage, AboutMePage, HomePage, MyPortfolio, ImprintPage, Footer };

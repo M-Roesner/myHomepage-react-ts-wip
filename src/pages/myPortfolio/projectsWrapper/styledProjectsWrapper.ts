e1b3cefa0f55793c90903e4e0a79f05ea26e406f
M-Roesner <markus_roesner@web.de>
2024-06-01 00:39:38 +0200

    TESTing grid design for StyledProjectsGrid

--- src/pages/myPortfolio/projectsWrapper/styledProjectsWrapper.ts
+++ src/pages/myPortfolio/projectsWrapper/styledProjectsWrapper.ts
@@ -2,22 +2,23 @@ import styled from "styled-components";
 
 export const StyledProjectsGrid = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
+  grid-template-columns: repeat(3, minmax(350px, 1fr));
+  /* grid-template-columns: repeat(auto-fit, minmax(350px, 1fr)); */
+
   gap: ${(props) => props.theme.sizes.common.gap};
 
   /* grid-template-columns: 1fr 1fr 1fr 1fr; */
-  /* @media (max-width: 1200px) {
+  @media (max-width: 1200px) {
+    /* grid-template-columns: repeat(auto-fit, minmax(300px, 1fr)); */
     grid-template-columns: 1fr 1fr 1fr;
   }
-  @media (max-width: 992px) {
-    grid-template-columns: 1fr 1fr 1fr;
-  }
-  @media (max-width: 768px) {
+  @media (max-width: 1000px) {
+    /* grid-template-columns: repeat(2, minmax(350px, 1fr)); */
     grid-template-columns: 1fr 1fr;
   }
-  @media (max-width: 576px) {
-    grid-template-columns: 1fr;
-  } */
+  @media (max-width: 600px) {
+    grid-template-columns: repeat(1, minmax(350px, 1fr));
+  }
 
   // Variate mit display flex
   /* display: flex;

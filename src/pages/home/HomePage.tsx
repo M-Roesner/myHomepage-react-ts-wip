2f07c3350f7988ac0cef3ffdd11ec9e51e15f58c
M-Roesner <markus_roesner@web.de>
2024-06-03 00:10:30 +0200

    Added InfoCard to the HomePage.

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,10 +1,13 @@
 // Components
 import PageLayout from "../../components/custom/layout/pageLayout/PageLayout";
+import { CardParagraph } from "../../components/custom/card/cardParagraph/styledCardParagraphStyle";
+import InfoCardSection from "./sections/infoCardSection/InfoCardSection";
 
 const HomePage = () => {
   return (
-    <PageLayout useBackButton={false} headlineText="Home Page">
-      some content - wip !
+    <PageLayout useBackButton={false} headlineText="Willkommen auf meiner Seite!">
+      <CardParagraph>Auf dieser Seite können Sie vieles über mich und meine Projekte erfahren.</CardParagraph>
+      <InfoCardSection />
     </PageLayout>
   );
 };

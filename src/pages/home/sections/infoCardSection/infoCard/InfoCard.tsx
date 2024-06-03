6fc5c6a03eb48aacb0487dbbd43eee273fd0cb1b
M-Roesner <markus_roesner@web.de>
2024-06-03 18:39:57 +0200

    Added image as background image or normal image.

--- src/pages/home/sections/infoCardSection/infoCard/InfoCard.tsx
+++ src/pages/home/sections/infoCardSection/infoCard/InfoCard.tsx
@@ -1,18 +1,41 @@
 import CardHeadline from "../../../../../components/custom/card/cardHeadline/CardHeadline";
+import { StyledCardImage } from "../../../../../components/custom/card/cardImage/styledCardImage";
 import { CardParagraph } from "../../../../../components/custom/card/cardParagraph/styledCardParagraphStyle";
 import { ERouteType } from "../../../../../routes/router";
 import { StyledInfoCard_NavLink } from "./styledInfoCard";
+
+export type InfoCardImageType = {
+  imgSrc: string;
+  imgAlt?: string;
+};
 
 type InfoCardProps = {
   url: ERouteType;
   title: string;
   descriptions: string[];
-  bgImage?: string;
+  image?: InfoCardImageType;
+  isBGImage?: boolean;
 };
-const InfoCard = ({ url, title, descriptions }: InfoCardProps) => {
-  // TODO: BgImage: set a background image to the card.
+
+/**
+ * Renders an info card component with optional background image.
+ *
+ * @param {Object} props - The props object.
+ * @param {ERouteType} props.url - The route URL the card navigates to.
+ * @param {string} props.title - The title of the card.
+ * @param {string[]} props.descriptions - An array of descriptions for the card.
+ * @param {InfoCardImageType} [props.image] - An optional object containing information about the card's image.
+ * @param {string} props.image.imgSrc - The source URL of the card's image.
+ * @param {string} [props.image.imgAlt] - Optional alternate text for the card's image.
+ * @param {boolean} [props.isBGImage] - Boolean flag indicating whether the image should be used as a background image.
+ *
+ * @returns {React.ReactNode} - A styled info card component.
+ */
+
+const InfoCard = ({ url, title, descriptions, image, isBGImage }: InfoCardProps): React.ReactNode => {
   return (
-    <StyledInfoCard_NavLink $display="block" to={`/${url}`}>
+    <StyledInfoCard_NavLink $bgImage={isBGImage ? image?.imgSrc : undefined} $display="block" to={`/${url}`}>
+      {!isBGImage && image && <StyledCardImage src={image.imgSrc} alt={image.imgAlt} />}
       <CardHeadline level={4}>{title}</CardHeadline>
       {descriptions.map((description, index) => (
         <CardParagraph key={index}>{description}</CardParagraph>
--- src/pages/home/sections/infoCardSection/infoCard/styledInfoCard.ts
+++ src/pages/home/sections/infoCardSection/infoCard/styledInfoCard.ts
@@ -1,8 +1,12 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import {
   NavLinkButtonWrapperType,
   navLinkButtonWrapper_Default,
 } from "../../../../../components/custom/button/navLinkButton/styledNavLinkButton";
+
+type StyledInfoCard_NavLinkProps = NavLinkButtonWrapperType & {
+  $bgImage?: string;
+};
 
 /**
  * Styled component for wrapping NavLink buttons, inheriting properties and styles from the default NavLink button wrapper.
@@ -12,13 +16,34 @@ import {
  * @param $display - Optional prop to set the display style of the NavLink button wrapper. Default is set to “inline”.
  * Example usage: `display: ${(props) => props.$display || "inline"};`
  */
-export const StyledInfoCard_NavLink = styled(navLinkButtonWrapper_Default)<NavLinkButtonWrapperType>`
+export const StyledInfoCard_NavLink = styled(navLinkButtonWrapper_Default)<StyledInfoCard_NavLinkProps>`
+  position: relative;
   display: ${(props) => props.$display || "inline"};
   padding: 10px;
   text-align: center;
   border: solid 1px ${(props) => props.theme.colors.common.borderColor};
+  z-index: 1;
 
   :first-child {
     margin-top: 0;
   }
+
+  ${(props) =>
+    props.$bgImage &&
+    css`
+      &::before {
+        content: "";
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-image: url(${props.$bgImage});
+        background-size: cover;
+        background-position: center;
+        opacity: 0.2;
+        filter: blur(2px);
+        z-index: 0;
+      }
+    `}
 `;

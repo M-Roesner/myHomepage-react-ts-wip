227301c21ae429667503ec5aee782de2b6f5e7c0
M-Roesner <markus_roesner@web.de>
2024-06-10 20:09:55 +0200

    Added ProgressWrapper to show the completed and planned features. -wip-

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -2,11 +2,13 @@
 import PageLayout from "../../components/custom/layout/pageLayout/PageLayout";
 import { CardParagraph } from "../../components/custom/card/cardParagraph/styledCardParagraphStyle";
 import InfoCardSection from "./sections/infoCardSection/InfoCardSection";
+import ProgressList from "./sections/progressListSection/ProgressListWrapper";
 
 const HomePage = () => {
   return (
     <PageLayout useBackButton={false} headlineText="Willkommen auf meiner Seite!">
       <CardParagraph>Auf dieser Seite können Sie vieles über mich und meine Projekte erfahren.</CardParagraph>
+      <ProgressList />
       <InfoCardSection />
     </PageLayout>
   );
